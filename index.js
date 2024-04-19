0626e52cb259f0d96f1b529ffc973e299f6f9b8b
Caroline Conte <carolinecontee@gmail.com>
2024-04-19 20:15:06 +0200

    teste

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const fs = require('fs')
 
 const rotaFavoritos = require('./routes/favoritos')
 const rotaLivro = require('./routes/books')
 
 app.use(express.json())
-app.use(cors({origin:'*'}))
+app.use(cors({ origin: '*' }))
 
 app.use('/livros', rotaLivro);
 app.use('/favoritos', rotaFavoritos);
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   return res.json("Hello Word")
 })
+
+app.get("/livros", (req, res) => {
+  const books = JSON.parse(fs.readFileSync('books.json'))
+  res.send(books)
+})
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
