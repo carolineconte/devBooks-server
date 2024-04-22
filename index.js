f0006052b77dbf168558f75731805f81e5418ca9
Caroline Conte <carolinecontee@gmail.com>
2024-04-22 12:05:39 +0200

    404

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const rotaLivro = require('./routes/books')
 app.use(express.json())
 app.use(cors({ origin: '*' }))
 
-app.use('/', rotaLivro);
 app.use('/livros', rotaLivro);
 app.use('/favoritos', rotaFavoritos);
 app.use('/myfavorites', rotaFavoritos);
+
+// Middleware para lidar com rotas não encontradas (erro 404)
+app.use((req, res, next) => {
+  res.status(404).send("Desculpe, a página que você está procurando não foi encontrada.");
+});
 
 
 const port = process.env.PORT || 8000;
